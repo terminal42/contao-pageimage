668d92858ed892f753d44832808bab0e208b796f
Andreas Schempp <andreas@schempp.ch>
2010-12-29 08:13:34 +0000

    French translation for extension "pageimage"

---
+++ languages/fr/tl_page.php
@@ -0,0 +1,43 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Andreas Schempp 2009-2010
+ * @author     Lionel Maccaud <lionel.maccaud@gmail.com>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @version    $Id$
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_page']['pageImage']			= array('Image pour cette page', 'Veuillez, s\'il vous plaît, choisir une image pour cette page. Si vous n\'avez rien choisi, l\'image de la page parent sera utilisé.');
+$GLOBALS['TL_LANG']['tl_page']['pageImageJumpTo']	= array('Image en tant que lien pour cette page', 'Vous pouvez ajouter un lien vers une autre page de Contao en cliquant sur l\'image.');
+$GLOBALS['TL_LANG']['tl_page']['pageImageAlt']		= array('Texte alternatif', 'Un site internet accessible doit toujours fournir un texte alternatif pour les images qui sera une courte description de leur contenu.');
+$GLOBALS['TL_LANG']['tl_page']['pageImageTitle']	= array('Titre du lien', 'Entrez un titre pour le lien. Si vous n\'insérez pas un titre, le titre de la page cible sera utilisé.');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_page']['image_legend']		= 'Page image';
+
