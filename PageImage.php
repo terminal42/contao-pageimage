ca317871caf7710cdb2c7d60bbfa53a91d9da9c4
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-09-13 15:00:51 +0200

    CS

--- PageImage.php
+++ PageImage.php
@@ -22,125 +22,124 @@
 class PageImage extends Frontend
 {
 
-	/**
-	 * Current object instance (Singleton)
-	 * @var object
-	 */
-	protected static $objInstance;
+    /**
+     * Current object instance (Singleton)
+     * @var object
+     */
+    protected static $objInstance;
 
-	/**
-	 * Cache
-	 */
+    /**
+     * Cache
+     */
 	protected $arrCache = null;
 
 
-	/**
-	 * Prevent cloning of the object (Singleton)
-	 */
-	final private function __clone() {}
+    /**
+     * Prevent cloning of the object (Singleton)
+     */
+    final private function __clone() {}
 
 
-	/**
-	 * Prevent direct instantiation (Singleton)
-	 */
-	protected function __construct()
-	{
-		parent::__construct();
-	}
+    /**
+     * Prevent direct instantiation (Singleton)
+     */
+    protected function __construct()
+    {
+        parent::__construct();
+    }
 
 
-	/**
-	 * Instantiate the class
-	 *
-	 * @return object
-	 */
-	public static function getInstance()
-	{
-		if (!is_object(self::$objInstance)) {
-			self::$objInstance = new PageImage();
-		}
+    /**
+     * Instantiate the class
+     *
+     * @return object
+     */
+    public static function getInstance()
+    {
+        if (!is_object(self::$objInstance)) {
+            self::$objInstance = new PageImage();
+        }
 
-		return self::$objInstance;
-	}
-
-
-	/**
-	 * Replace "pageimage" inserttag
-	 * @param string
-	 * @return mixed
-	 */
-	public function replaceTags($strTag)
-	{
-		$arrTag = trimsplit('::', $strTag);
-
-		switch($arrTag[0])
-		{
-			case 'pageimage':
-				$arrTag[0] = 'pageimage_src';
-				// Do NOT add a break;
-
-			case 'pageimage_alt':
-			case 'pageimage_title':
-			case 'pageimage_href':
-				$arrImage = $this->getPageImage(false, $arrTag[1]);
-
-				if ($arrImage === false) {
-					return '';
-				}
-
-				return $arrImage[str_replace('pageimage_', '', $arrTag[0])];
-
-			default:
-				return false;
-		}
-	}
+        return self::$objInstance;
+    }
 
 
-	/**
-	 * Search for the current page image
-	 * @param	int
-	 * @param	bool
-	 * @return	mixed
-	 */
-	public function getPageImage($blnMultipleImages, $intIndex=0, $intTotal=null, $blnInherit=true)
-	{
-		$intIndex = (int) $intIndex;
+    /**
+     * Replace "pageimage" inserttag
+     * @param   string
+     * @return  mixed
+     */
+    public function replaceTags($strTag)
+    {
+        $arrTag = trimsplit('::', $strTag);
 
-		if (is_null($this->arrCache[$intIndex])) {
-			global $objPage;
+        switch($arrTag[0])
+        {
+            case 'pageimage':
+                $arrTag[0] = 'pageimage_src';
+                // Do NOT add a break;
+
+            case 'pageimage_alt':
+            case 'pageimage_title':
+            case 'pageimage_href':
+                $arrImage = $this->getPageImage(false, $arrTag[1]);
+
+                if ($arrImage === false) {
+                    return '';
+                }
+
+                return $arrImage[str_replace('pageimage_', '', $arrTag[0])];
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Search for the current page image
+     * @param    int
+     * @param    bool
+     * @return    mixed
+     */
+    public function getPageImage($blnMultipleImages, $intIndex=0, $intTotal=null, $blnInherit=true)
+    {
+        $intIndex = (int) $intIndex;
+
+        if (null === $this->arrCache) {
+            global $objPage;
 
 			$this->arrCache[$intIndex] = false;
 
-			// Current page has an image
-			if ($objPage->pageImage != '') {
-				$this->arrCache[$intIndex] = $this->parsePage($blnMultipleImages, $objPage, $intIndex, $intTotal);
-			}
+            // Current page has an image
+            if ($objPage->pageImage != '') {
+                $this->arrCache[$intIndex] = $this->parsePage($blnMultipleImages, $objPage, $intIndex, $intTotal);
+            }
 
-			// Walk the trail
-			elseif ($blnInherit && count($objPage->trail)) {
+            // Walk the trail
+            elseif ($blnInherit && count($objPage->trail)) {
 				$objTrail = $this->Database->execute("SELECT * FROM tl_page WHERE id IN (" . implode(',', $objPage->trail) . ") ORDER BY id=" . implode(' DESC, id=', array_reverse($objPage->trail)) . " DESC");
 
-				while ($objTrail->next()) {
-					if ($objTrail->pageImage != '') {
-						$this->arrCache[$intIndex] = $this->parsePage($blnMultipleImages, $objTrail, $intIndex, $intTotal);
-						break;
-					}
-				}
-			}
-		}
+                while ($objTrail->next()) {
+                    if ($objTrail->pageImage != '') {
+                        $this->arrCache[$intIndex] = $this->parsePage($blnMultipleImages, $objTrail, $intIndex, $intTotal);
+                        break;
+                    }
+                }
+            }
+        }
 
-		return $this->arrCache[$intIndex];
-	}
+        return $this->arrCache[$intIndex];
+    }
 
 
-	/**
-	 * Parse the given page and return the image information
-	 * @param	Database_Result
-	 * @param	int
-	 * @return	array
-	 */
-	protected function parsePage($blnMultipleImages, $objPage, $intIndex = null, $intTotal = null)
-	{
+    /**
+     * Parse the given page and return the image information
+     * @param    Database_Result
+     * @param    int
+     * @return    array
+     */
+    protected function parsePage($blnMultipleImages, $objPage, $intIndex = null, $intTotal = null)
+    {
         $arrOptions = array();
         $arrOrder = array_filter(explode(',', $objPage->pageImageOrder));
 
@@ -195,6 +194,6 @@ class PageImage extends Frontend
             return $arrImages[$intIndex];
         }
 
-		return $arrImages;
-	}
+        return $arrImages;
+    }
 }
