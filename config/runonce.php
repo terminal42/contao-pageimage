e4f1411f839f01df72ecba7b991de89f3feaacd5
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2014-02-21 09:30:16 +0100

    Added the runonce upgrade file (see #8)

---
+++ config/runonce.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * pageimage Extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2009-2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-pageimage
+ */
+
+
+class ImagePageUpgrade
+{
+
+    public function run()
+    {
+        if (version_compare(VERSION, '3.2', '>=') && \Database::getInstance()->fieldExists('pageImage', 'tl_page'))
+        {
+            \Database\Updater::convertSingleField('tl_page', 'pageImage');
+        }
+    }
+}
+
+$objUpgrade = new ImagePageUpgrade();
+$objUpgrade->run();
