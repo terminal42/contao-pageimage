d0dcf3ce52d3d7626cd4007f2bb946d54cb0c9ed
Simon Wohler <mail@bekanntmacher.ch>
2014-02-28 07:09:10 +0100

    Update tl_page.php
    Contao instead of Typolight

--- languages/de/tl_page.php
+++ languages/de/tl_page.php
@@ -23,7 +23,7 @@
  * Fields
  */
 $GLOBALS['TL_LANG']['tl_page']['pageImage']			= array('Bild für diese Seite', 'Wählen Sie ein Bild für diese Seite. Wenn Sie nichts angeben wird ggf. das Bild der übergeordneten Seite verwendet.');
-$GLOBALS['TL_LANG']['tl_page']['pageImageJumpTo']	= array('Bild-Link für diese Seite', 'Sie können beim Klicken auf das Seitenbild auf eine andere TYPOlight-Seite weiterleiten.');
+$GLOBALS['TL_LANG']['tl_page']['pageImageJumpTo']	= array('Bild-Link für diese Seite', 'Sie können beim Klicken auf das Seitenbild auf eine andere Contao-Seite weiterleiten.');
 $GLOBALS['TL_LANG']['tl_page']['pageImageAlt']		= array('Alternativer Text', 'Eine barrierefreie Webseite sollte immer einen alternativen Text für Bilder mit einer kurzen Beschreibung deren Inhalts enthalten.');
 $GLOBALS['TL_LANG']['tl_page']['pageImageTitle']	= array('Link-Titel', 'Geben Sie einen Titel für den Link ein. Ohne Eingabe wird der Titel der Bild-Link-Seite verwendet.');
 
