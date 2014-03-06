99fc0d6c2e1ab2d2a3aeaf8ebbf10d15df920b63
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-03-06 11:02:55 +0100

    Added german translation

--- languages/de/modules.php
+++ languages/de/modules.php
@@ -19,3 +19,4 @@ $GLOBALS['TL_LANG']['MOD']['pageImage'] = array('Bild der Seite', '');
  * Frontend modules
  */
 $GLOBALS['TL_LANG']['FMD']['pageImage']	= array('Bild der Seite', 'Zeigt das Bild einer Seite an. Bilder können von übergeordneten Seiten übernommen werden.');
+$GLOBALS['TL_LANG']['FMD']['backgroundImage'] = array('Hintergrund-Bild', 'Gibt das Bild der Seite als Hintergrundbild aus.');
