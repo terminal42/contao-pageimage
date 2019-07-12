5a49f9b21845206ae30fcba02eff89117ef49b7d
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2019-07-12 13:57:27 +0200

    Change translation of fmd.

--- languages/de/modules.php
+++ languages/de/modules.php
@@ -19,4 +19,4 @@ $GLOBALS['TL_LANG']['MOD']['pageImage'] = array('Bild der Seite', '');
  * Frontend modules
  */
 $GLOBALS['TL_LANG']['FMD']['pageImage']	= array('Bild der Seite', 'Zeigt das Bild einer Seite an. Bilder können von übergeordneten Seiten übernommen werden.');
-$GLOBALS['TL_LANG']['FMD']['backgroundImage'] = array('Hintergrund-Bild', 'Gibt das Bild der Seite als Hintergrundbild aus.');
+$GLOBALS['TL_LANG']['FMD']['backgroundImage'] = array('Hintergrundbild der Seite', 'Gibt das Bild der Seite als Hintergrundbild aus.');
