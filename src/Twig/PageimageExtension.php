01c8dc2bc2bc86342d1d809bbaa0fb58eeb3309d
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-12-12 19:07:44 +0100

    Add method return argument

--- src/Twig/PageimageExtension.php
+++ src/Twig/PageimageExtension.php
@@ -9,7 +9,7 @@ use Twig\TwigFunction;
 
 class PageimageExtension extends AbstractExtension
 {
-    public function getFunctions()
+    public function getFunctions(): array
     {
         return [
             new TwigFunction('pageimage', [PageimageRuntime::class, '__invoke']),
